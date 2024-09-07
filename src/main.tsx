eab533c2eec32a77d1eaeb2e3a2ee724582b2f7b
horsmanayo <oniademola001@gmail.com>
2024-09-07 21:11:26 +0100

    minor-fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux"; 
-import { store } from "./redux/store";  
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}> 
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
