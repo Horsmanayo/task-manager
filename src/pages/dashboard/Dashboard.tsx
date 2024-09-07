44653c3dab1f5dd6d0cb705cf27e4241bf96969c
horsmanayo <oniademola001@gmail.com>
2024-09-07 11:37:18 +0100

    fixes

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,17 @@
-
-import Sidebar from "../../components/Sidebar"
+import { Route, Routes } from "react-router-dom";
+import Sidebar from "../../component/Sidebar";
+import Overview from "./Overview";
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className="flex">
       <Sidebar />
+      <Routes>
+        <Route path="/" element={<Overview />} />
+        <Route path="/overview" element={<Overview />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
