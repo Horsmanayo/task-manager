aa615b52e60736ff696d32575d6834bf6c9bf440
Adaeze Ugwumba <adaezeugwumba460@gmail.com>
2024-09-09 07:11:26 +0100

    Added a RightSide component to display TaskDetails

--- src/pages/dashboard/tasks/Tasks.tsx
+++ src/pages/dashboard/tasks/Tasks.tsx
@@ -1,16 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { tasks } from "../../../utils/tasks";
 import TaskCard from "./TaskCard";
 
-const Tasks = () => {
+// onTaskClick prop is a function passed from the Overview to capture when a task is clicked.
+
+const Tasks = ({ onTaskClick }: { onTaskClick: (task: any) => void }) => {
   return (
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
       {tasks.map((task) => (
         <TaskCard
           key={task.id}
-          taskImage={task.image}
+          image={task.taskImage}
           taskName={task.taskName}
           progress={task.progress}
           deadline={task.deadline}
+          onClick={() => onTaskClick(task)} 
         />
       ))}
     </div>
